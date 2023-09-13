e52d4b7e0162c3e0406f340cca59ffeca89e21da
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-09-13 15:16:14 +0200

    new pyplay

--- acceptance_tests/specs/book_club_spec.py
+++ acceptance_tests/specs/book_club_spec.py
@@ -1,15 +1,10 @@
-from acceptance_tests.actions.add_actor_as_a_new_member import add_actor_as_new_member
-from acceptance_tests.actions.make_myself_president import make_myself_president
-from acceptance_tests.actions.welcome_received import welcome_received
+from acceptance_tests import actions
+from pyplay.action_executor import find_executors_in_module
 from pyplay.logger import pyplay_logger
 from pyplay.play_execution import pyplay_spec
 
 book_club_spec = pyplay_spec(
     narrator=pyplay_logger(),
     prop_factories={},
-    action_executors=[
-        make_myself_president,
-        welcome_received,
-        add_actor_as_new_member
-    ]
+    action_executors=find_executors_in_module(actions)
 )
