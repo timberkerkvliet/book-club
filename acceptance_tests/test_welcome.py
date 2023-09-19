846f46f271f7d6a796202577ffd51f8d5db173c2
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-09-19 14:30:09 +0200

    test welcome

--- acceptance_tests/test_welcome.py
+++ acceptance_tests/test_welcome.py
@@ -1,13 +1,13 @@
 from unittest import IsolatedAsyncioTestCase
 
-from acceptance_tests.specs.book_club_spec import book_club_spec
+from acceptance_tests.book_club_spec import book_club_spec
 from pyplay.play import CharacterCall
 from acceptance_tests.actions.add_actor_as_a_new_member import AddActorAsANewMember
 from acceptance_tests.actions.make_myself_president import MakeMyselfPresident
 from acceptance_tests.actions.welcome_received import WelcomeReceived
 
 
-class Test(IsolatedAsyncioTestCase):
+class TestWelcome(IsolatedAsyncioTestCase):
     @book_club_spec
     def test_added_member_receives_welcome(self, character: CharacterCall) -> None:
         timber = character('John').performs(MakeMyselfPresident())
