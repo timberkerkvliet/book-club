e136b5e131f3ebe121eb8d5908878503c30c1418
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-09-22 15:04:57 +0200

    entrypoint

---
+++ book_club/entrypoint.py
@@ -0,0 +1,14 @@
+import asyncio
+
+from book_club.request_handler import request_handler
+from book_club.starlette_adapter import StarletteRequestHandler, run_server
+
+
+async def run():
+    adapter = StarletteRequestHandler(
+        request_handler=request_handler()
+    )
+    await run_server(adapter, host='0.0.0.0', port=8000)
+
+
+asyncio.run(run())
