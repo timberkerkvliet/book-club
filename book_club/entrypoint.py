9eca6ba264ab408f8bdcac8017ccb34ddaede77d
Timber Kerkvliet <timberkerkvliet@gmail.com>
2023-09-23 16:38:31 +0200

    wait for sigterm

--- book_club/entrypoint.py
+++ book_club/entrypoint.py
@@ -1,4 +1,5 @@
 import asyncio
+import signal
 from asyncio import Event
 
 from book_club.app import app
@@ -6,6 +7,8 @@ from book_club.app import app
 
 async def run():
     async with app():
-        await Event().wait()
+        event = Event()
+        signal.signal(signal.SIGTERM, event.set)
+        await event.wait()
 
 asyncio.run(run())
