94792e3ef42ac4e7ff9b3928d4be88bc1023ee26
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-09-25 14:56:51 +0200

    fix app

--- book_club/app.py
+++ book_club/app.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 
 from book_club.app_context import AppContext
 from book_club.authenticate import authenticate
-from book_club.request_handler import request_handler
+from book_club.request_handler import command_handlers, query_handlers, request_handler
 from book_club.starlette_adapter import starlette_server, StarletteRequestHandler
 
 
@@ -16,7 +16,9 @@ async def app(is_fake: bool = False) -> AsyncGenerator[AppContext, None]:
                 starlette_server(
                     adapter=StarletteRequestHandler(
                         request_handler=request_handler(context),
-                        authenticator=authenticate
+                        authenticator=authenticate,
+                        command_types=set(command_handlers().keys()),
+                        query_types=set(query_handlers().keys())
                     ),
                     host='0.0.0.0',
                     port=80
