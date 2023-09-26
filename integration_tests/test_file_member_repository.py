f8400c487a307b8dd9f2b54d29d3577f1368af8f
Timber Kerkvliet <timberkerkvliet@gmail.com>
2023-09-26 07:36:56 +0200

    cleanup

--- integration_tests/test_file_member_repository.py
+++ integration_tests/test_file_member_repository.py
@@ -1,3 +1,4 @@
+import os
 from unittest import IsolatedAsyncioTestCase
 
 from book_club.mail_address import MailAddress
@@ -5,16 +6,22 @@ from book_club.member_list.member import Member
 from book_club.member_list.member_repository import FileMemberRepository
 from book_club.name import Name
 
+FILE_PATH = 'test_file'
+
 
 class TestFileMemberRepository(IsolatedAsyncioTestCase):
+    def tearDown(self) -> None:
+        if os.path.isfile(FILE_PATH):
+            os.remove(FILE_PATH)
+
     async def test_no_members(self):
-        repository = FileMemberRepository('hey')
+        repository = FileMemberRepository(FILE_PATH)
         member_list = await repository.get_member_list()
 
         self.assertEqual(len(member_list), 0)
 
     async def test_load_and_save(self):
-        repository = FileMemberRepository('hey')
+        repository = FileMemberRepository(FILE_PATH)
         member_list = await repository.get_member_list()
         member_list.add(
             Member(
@@ -23,6 +30,6 @@ class TestFileMemberRepository(IsolatedAsyncioTestCase):
             )
         )
         await repository.save()
-        repository = FileMemberRepository('hey')
+        repository = FileMemberRepository(FILE_PATH)
         member_list = await repository.get_member_list()
         self.assertEqual(len(member_list), 1)
