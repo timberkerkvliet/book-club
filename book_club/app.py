d58f84d7d56796d5709d55c4d224c8bcd482b34b
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-09-26 10:35:06 +0200

    fix app

--- book_club/app.py
+++ book_club/app.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from functools import lru_cache
 from typing import AsyncGenerator
 
-from book_club.app_context import AppContext
+from book_club.app_context import AppContext, app_resource
 from book_club.authenticate import authenticate
 from book_club.query_handlers import query_handlers
 from book_club.command_handlers import command_handlers
@@ -10,23 +10,26 @@ from book_club.request_handler import RequestHandler
 from book_club.starlette_adapter import starlette_server, StarletteRequestHandler
 
 
+@app_resource
+def starlette_resource(app_context):
+    return starlette_server(
+        adapter=StarletteRequestHandler(
+            request_handler=request_handler(app_context),
+            authenticator=authenticate,
+            command_types=set(command_handlers().keys()),
+            query_types=set(query_handlers().keys())
+        ),
+        host='0.0.0.0',
+        port=80
+    )
+
+
 @asynccontextmanager
 async def app(is_fake: bool = False) -> AsyncGenerator[AppContext, None]:
     context = AppContext(is_fake=is_fake)
     async with context:
         if not is_fake:
-            await context.join(
-                starlette_server(
-                    adapter=StarletteRequestHandler(
-                        request_handler=request_handler(context),
-                        authenticator=authenticate,
-                        command_types=set(command_handlers().keys()),
-                        query_types=set(query_handlers().keys())
-                    ),
-                    host='0.0.0.0',
-                    port=80
-                )
-            )
+            await starlette_resource(context)
         yield context
 
 
