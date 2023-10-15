9aad7d4aa00b6b1a5445963f62145fe5480bcf4d
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-10-15 10:56:57 +0200

    cleanup

--- acceptance_tests/test_kick_member.py
+++ acceptance_tests/test_kick_member.py
@@ -1,14 +1,11 @@
 from unittest import IsolatedAsyncioTestCase
 
-from acceptance_tests.actions.can_not_kick_member import CanNotKickMember
 from acceptance_tests.actions.is_a_member import IsAMember
 from acceptance_tests.actions.is_not_a_member import IsNotAMember
 from acceptance_tests.actions.leave_club import KickMember
 from acceptance_tests.arrangements.club_with_president_and_member import arrange_club_with_president_and_member
 from acceptance_tests.book_club_spec import book_club_spec
 from pyplay.play import CharacterCall
-from acceptance_tests.actions.add_member import AddMember
-from acceptance_tests.actions.become_president import BecomePresident
 
 
 class TestKickMember(IsolatedAsyncioTestCase):
