32f1fc3d67832b665b53bff0a3b10e6e7cd275ad
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-10-16 14:30:01 +0200

    members get notified about book election

--- book_club/book_election/start_book_election.py
+++ book_club/book_election/start_book_election.py
@@ -17,3 +17,9 @@ async def start_book_election(
 ) -> None | Failure:
     if request_context.invoker != President():
         return Failure()
+
+    await mail_all_members(
+        request_context=request_context,
+        subject='Book Election',
+        content='\n'.join(command.book_names)
+    )
