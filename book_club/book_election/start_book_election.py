c3f2737b72117022f1dc80b99d80ae1334643262
Timber Kerkvliet <tkerkvliet@cloudsuite.com>
2023-10-17 20:06:43 +0200

    members can not start book election

--- book_club/book_election/start_book_election.py
+++ book_club/book_election/start_book_election.py
@@ -15,5 +15,7 @@ async def start_book_election(
     command: StartBookElection,
     request_context: RequestContext
 ) -> None | Failure:
-    pass
+    if request_context.invoker != President():
+        return Failure()
+
 
